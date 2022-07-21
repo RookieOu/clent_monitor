7f7cf591e03c8e1333d9d490be6262a4c64c1c58
yoyoyo1214 <66930941+yoyoyo1214@users.noreply.github.com>
2022-07-21 20:00:54 +0800

    Delete venv directory

--- venv/lib/python3.9/site-packages/pkg_resources/tests/data/my-test-package-source/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-@File : __init__.py.py   
-@Contact : 1175774748@qq.com
- 
-@Modify Time      @Author    @Version    @Desciption
-------------      -------    --------    -----------
-2022/7/21 10:43 上午   yanou      1.0         None
-"""
